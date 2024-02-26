acd3a8e8e5b1e73e7e14425a0b86405aeff09c1b
the-revelator <59324459+the-revelator@users.noreply.github.com>
2024-02-25 17:16:46 -0800

    updating newsletter

--- script.js
+++ script.js
@@ -56,3 +56,5 @@ updateCountdownDisplay();
 // Update the countdown every second
 const x = setInterval(updateCountdownDisplay, 1000);
 });
+
+<script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/8f2e63d57d0c2e1a06c3b1e2f/7275a3324c371eaa02df10591.js");</script>

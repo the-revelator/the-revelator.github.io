a6b21a75addf83adc8b0c8e4eb298df03fbae761
the-revelator <59324459+the-revelator@users.noreply.github.com>
2024-05-26 18:59:16 -0700

    monthly refresh

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Set up countdown
   // Set the date and time for the countdown
-    const countdownDate = new Date("May 26, 2024 15:00:00 PDT").getTime();
+    const countdownDate = new Date("June 30, 2024 17:00:00 PDT").getTime();
 
 // Function to update the display based on the countdown
 function updateCountdownDisplay() {

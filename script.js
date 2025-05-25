e38df3c78eef01dd4a5e4f5d54350de8cef160d1
the-revelator <59324459+the-revelator@users.noreply.github.com>
2025-05-25 15:44:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Set up countdown
   // Set the date and time for the countdown
-    const countdownDate = new Date("May 25, 2025 17:00:00 PST").getTime();
+    const countdownDate = new Date("May 25, 2025 17:00:00 PDT").getTime();
 
 // Function to update the display based on the countdown
 function updateCountdownDisplay() {
